77daf482c5c71299a55b33f1aa3815821c325afb
Eugen Podaru <eugen.podaru@live.com>
2015-05-04 20:16:54 +0200

    add grunt task to install bower packages; change the deployment script for azure to call the grunt task;

---
+++ Resume/Gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        clean: ["Content/lib"],
+        "bower-install-simple": {
+            options: {
+                color: true
+            },
+            "prod": {
+                options: {
+                    production: true
+                }
+            },
+            "dev": {
+                options: {
+                    production: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
+    grunt.loadNpmTasks("grunt-bower-install-simple");
+
+    grunt.registerTask("bower-install", ["clean", "bower-install-simple"]);
+};
