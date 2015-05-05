846cd12bb0aa426d29d3bace5a50592d6696b39a
Eugen Podaru <eugen.podaru@live.com>
2015-05-06 00:58:37 +0200

    clean the deployment script

--- Scripts/deploy.cmd
+++ Scripts/deploy.cmd
@@ -1,5 +1,3 @@
-@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
-
 setlocal enabledelayedexpansion
 
 IF NOT DEFINED DEPLOYMENT_SOURCE (
@@ -7,22 +5,5 @@ IF NOT DEFINED DEPLOYMENT_SOURCE (
 )
 
 call powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File "%DEPLOYMENT_SOURCE%\Scripts\deploy.ps1"
-IF !ERRORLEVEL! NEQ 0 goto error
 
-goto end
-
-:error
 endlocal
-echo An error has occurred during web site deployment.
-call :exitSetErrorLevel
-call :exitFromFunction 2>nul
-
-:exitSetErrorLevel
-exit /b 1
-
-:exitFromFunction
-()
-
-:end
-endlocal
-echo Finished successfully.
