cdbcf52293eeda9b91349aeaee804e7ee4d86b93
Eugen Podaru <eugen.podaru@live.com>
2015-05-05 23:38:13 +0200

    improve the ps script error logging

--- Scripts/deploy.ps1
+++ Scripts/deploy.ps1
@@ -2,26 +2,27 @@
 # -------------
 
 # Write an error message and exit with a code
-function Error(){
-    Write-Output "An error has occurred during web site deployment.";
+function Error($msg){
+    Write-Error "An error has occurred during web site deployment.";
+    Write-Error $msg;
     Exit 1;
 }
 
 # Check the last exit code, and in case it's different than 0, exit with an error
-function CheckLastExitCode(){
+function CheckLastExitCode($msg){
     if($LASTEXITCODE -ne 0){
-        Error;
+        Error $msg;
     }
 }
 
 # Create the directory. If it exists, delete it first
 function CreateDirectory($dir){
     if(Test-Path -Path $dir){
-        Remove-Item -Recurse -Force -Path $dir;
-        CheckLastExitCode;
+        Remove-Item -Recurse -Force -Path $dir | Out-Null;
+        CheckLastExitCode "Could not remove directory $dir";
     }
-    New-Item -Force -ItemType directory -Path $dir;
-    CheckLastExitCode;
+    New-Item -Force -ItemType directory -Path $dir  | Out-Null;
+    CheckLastExitCode "Could not create directory $dir";
 }
 
 # Prerequisites
@@ -29,8 +30,7 @@ function CreateDirectory($dir){
 
 # Verify node.js installed
 if(-NOT [bool](Get-Command -Name "node" -ErrorAction SilentlyContinue)){
-    Write-Output "Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.";
-    Error;
+    Error "Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.";
 }
 
 # get the paths from the environment variables
@@ -62,7 +62,7 @@ if(Test-Path Env:\KUDU_SYNC_CMD){
 } else {
     Write-Output "Installing Kudu Sync";
     & npm install kudusync -g --silent;
-    CheckLastExitCode;
+    CheckLastExitCode "Could not install KuduSync";
 }
 
 $DeploymentTemp = "$env:TEMP\__deployTemp" + (Get-Random);
@@ -93,55 +93,55 @@ CreateDirectory $PostDeploymentTarget;
 # 1. Restore NuGet packages
 Write-Output "Restoring NuGet packages...";
 & nuget restore "$DeploymentSource\Resume.sln";
-CheckLastExitCode;
+CheckLastExitCode "Could not restore NuGet packages";
 
 # 2. Install npm packages, bower, grunt, and bower packages
 Push-Location "$DeploymentSource\Resume";
 
 Write-Output "Installing npm packages...";
 & npm install;
-CheckLastExitCode;
+CheckLastExitCode "Installing npm packages failed";
 
 Write-Output "Running npm dedupe...";
 & npm dedupe;
-CheckLastExitCode;
+CheckLastExitCode "Running npm dedupe failed";
 
 Write-Output "Installing grunt-cli...";
 & npm install grunt-cli -g;
-CheckLastExitCode;
+CheckLastExitCode "Installing grunt-cli failed";
 
 Write-Output "Installing bower...";
 & npm install bower -g;
-CheckLastExitCode;
+CheckLastExitCode "Installing bower failed";
 
 Write-Output "Running bower-install grunt task...";
 & grunt bower-install;
-CheckLastExitCode;
+CheckLastExitCode "Running bower-install task failed";
 
 Pop-Location;
 
 # 3. Build to the temporary path
 Write-Output "Build the solution";
 &$MSBuildPath "$DeploymentSource\Resume\Resume.csproj" /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="$DeploymentTemp";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="$DeploymentSource\.\\" $ScmBuildArgs;
-CheckLastExitCode;
+CheckLastExitCode "Build failed.";
 
 # 4. KuduSync
 Write-Output "KuduSync";
 &$KuduSyncCmd -v 50 -f "$DeploymentTemp" -t "$DeploymentTarget" -n "$NextManifestPath" -p "$PreviousManifestPath" -i ".git;.hg;.deployment;deploy.cmd";
-CheckLastExitCode;
+CheckLastExitCode "KuduSync failed";
 
 # 5. Copy the post deployment scripts
 Push-Location "$DeploymentSource";
 Write-Output "Copy the post deployment scripts...";
 & xcopy /I "$PostDeploymentSource" "$PostDeploymentTarget";
-CheckLastExitCode;
+CheckLastExitCode "Copying the post deployment scripts failed";
 Pop-Location;
 
 # Post deployment stub
 if(Test-Path Env:\POST_DEPLOYMENT_ACTION){
     Write-Output "Running the post deployment scripts...";
     &$env:POST_DEPLOYMENT_ACTION;
-    CheckLastExitCode;
+    CheckLastExitCode "Running the post deployment scripts failed";
 }
 
 Write-Output "Finished successfully.";
