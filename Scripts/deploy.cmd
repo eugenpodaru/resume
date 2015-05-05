cb64bab89315555b80530d4dbc77830851f5ae75
Eugen Podaru <eugen.podaru@live.com>
2015-05-05 23:08:29 +0200

    bypass execution policy when running the ps script

--- Scripts/deploy.cmd
+++ Scripts/deploy.cmd
@@ -2,4 +2,23 @@ IF NOT DEFINED DEPLOYMENT_SOURCE (
   SET DEPLOYMENT_SOURCE=%~dp0%.
 )
 
-powershell -noexit -file "%DEPLOYMENT_SOURCE%\Scripts\deploy.ps1"
+powershell -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File "%DEPLOYMENT_SOURCE%\Scripts\deploy.ps1"
+IF !ERRORLEVEL! NEQ 0 goto error
+
+goto end
+
+:error
+endlocal
+echo An error has occurred during web site deployment.
+call :exitSetErrorLevel
+call :exitFromFunction 2>nul
+
+:exitSetErrorLevel
+exit /b 1
+
+:exitFromFunction
+()
+
+:end
+endlocal
+echo Finished successfully.
