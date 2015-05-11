6a3bfb1e727f5aad7c628372f25bc1af7d5605ce
Eugen Podaru <eugen@netsociety.nl>
2015-05-11 20:11:32 +0200

    replace jQuery.Easing plugin

--- Resume/App_Start/BundleConfig.cs
+++ Resume/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@
             // javascript bundles
             bundles.Add(new ScriptBundle("~/bundles/js")
                 .Include("~/Content/lib/jquery/dist/jquery.min.js")
-                .Include("~/Content/lib/jquery-easing/jquery.easing.min.js")
+                .Include("~/Content/lib/jquery.easing/jquery.easing.min.js")
                 .Include("~/Content/lib/bootstrap/dist/js/bootstrap.min.js")
                 .Include("~/Content/Scripts/resume.min.js"));
 
